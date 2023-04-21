4d185d6ff6080eb310b3cfd40158124f602a208c
BiancaFED <118843956+BiancaFED@users.noreply.github.com>
2023-04-21 02:08:17 +0100

    Add files via upload

---
+++ header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/Logo.png";
+import hamburger from "../assets/hamburger.png";
+import basket from "../assets/basket.png";
+
+function Header() {
+  return (
+      <header className="header">
+        <Link to="/">
+          <img alt="hamburger icon" src={hamburger} className="icon" />
+        </Link>
+        <Link to="/">
+          <img alt="logo" src={logo} />
+        </Link>
+        <Link to="/">
+          <img alt="hamburger icon" src={basket} className="icon" />
+        </Link>
+        <nav className="navbar">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/">About</Link>
+            </li>
+            <li>
+              <Link to="/">Menu</Link>
+            </li>
+            <li>
+              <Link to="/Booking">Reservations</Link>
+            </li>
+            <li>
+              <Link to="/">Order Online</Link>
+            </li>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+  );
+}
+export default Header();
